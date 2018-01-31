e18c94afb3cacc26ed059ea7c0dfe408bc3a20b2
haosu <haosu@iflytek.com>
2018-01-31 16:59:04 +0800

    test list

--- helloworld.cc
+++ helloworld.cc
@@ -1,10 +1,58 @@
 #include <iostream>
+#include <list>
+#include <string>
+
+typedef struct retry_elem_s {
+    retry_elem_s(std::string* param_msg)
+        : num_retry(0)
+        , msg(param_msg)
+    {}
+
+    int num_retry;
+    std::string* msg;
+} stRetryElem_t;
+
+std::list<stRetryElem_t*> list_retry_;
 
 int main(int argc, char* argv[])
 {
-    std::cout << "hello world" << std::endl;
+    for(int i = 0; i < 10; ++i) {
+        stRetryElem_t* st_retry = new stRetryElem_t(&std::to_string(i));
+        list_retry_.push_back(st_retry);
+    }
 
-    std::cout << "merge test" << std::endl;
+    while(true) {
+    for (std::list<stRetryElem_t*>::iterator iter = list_retry_.begin(); iter != list_retry_.end(); ) {
+        if ((*iter) == nullptr || ((*iter)->msg) == nullptr) continue;
+
+        Job job;
+        ret = job.exec((*iter)->msg);
+
+        if (ATHENA_SUCCESS == ret) {
+            delete (*iter)->msg;
+            (*iter)->msg = nullptr;
+
+            delete (*iter);
+            (*iter) = nullptr;
+            
+            iter = list_retry_.erase(iter);
+        } else {
+            (*iter)->num_retry++;
+
+            if ((*iter)->num_retry > 3) {
+                delete (*iter)->msg;
+                (*iter)->msg = nullptr;
+
+                delete (*iter);
+                (*iter) = nullptr;
+
+                iter = list_retry_.erase(iter);
+            } else {
+                ++iter;
+            }
+        }
+    }
+    }
 
     return 0;
 }
